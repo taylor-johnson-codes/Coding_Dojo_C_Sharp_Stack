90ff69cdc992d950e54f58c4445f37f969b076ec
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-04 14:44:01 -0700

    working on assignment

--- 2_Language_Fundamentals/1_Language_Essentials/Puzzles/Program.cs
+++ 2_Language_Fundamentals/1_Language_Essentials/Puzzles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Puzzles
 {
@@ -6,7 +7,47 @@ namespace Puzzles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RandomArray();
         }
+        
+        /*
+        Create a function called RandomArray() that returns an integer array
+        - Place 10 random integer values between 5-25 into the array
+        - Print the min and max values of the array
+        - Print the sum of all the values
+        */
+        public static int[] RandomArray()
+        {
+            int[] randomArr = new int[] {5,6,7,8,9,10,11,12,13,14};
+            int max = randomArr[0];
+            int min = randomArr[0];
+            int sum = 0;
+            for (int i = 0; i < randomArr.Length; i++)
+            {
+                if (randomArr[i] > max)
+                {
+                    max = randomArr[i];
+                }
+                if (randomArr[i] < min)
+                {
+                    min = randomArr[i];
+                }
+                sum += randomArr[i];
+            }
+            Console.WriteLine($"Min: {min}, Max: {max}, Sum: {sum}");
+            return randomArr;
+        }
+        
+        /*
+        Create a function called TossCoin() that returns a string
+        - Have the function print "Tossing a Coin!"
+        - Randomize a coin toss with a result signaling either side of the coin 
+        - Have the function print either "Heads" or "Tails"
+        - Finally, return the result
+        
+        Create another function called TossMultipleCoins(int num) that returns a Double
+        - Have the function call the tossCoin function multiple times based on num value
+        - Have the function return a Double that reflects the ratio of head toss to total toss
+        */
     }
 }
