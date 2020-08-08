c7c7b12edddc4c56e75ce866e44505afa11c3762
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-08 13:06:45 -0700

    following demo

--- 3_ASP.NET_Core/1_ASP_MVC_I/Platform_Lecture_ASP_MVC_I/Controllers/HomeController.cs
+++ 3_ASP.NET_Core/1_ASP_MVC_I/Platform_Lecture_ASP_MVC_I/Controllers/HomeController.cs
@@ -49,6 +49,7 @@ namespace Platform_Lecture_ASP_MVC_I.Controllers  // added .Controllers to names
             // in both the Home and Shared folders inside the Views folder
         }
 
+
         // redirect to other controller methods rather than rendering a view:
         // (For example, when we handle a post request.)
         public RedirectToActionResult RedirectExample()
@@ -65,5 +66,49 @@ namespace Platform_Lecture_ASP_MVC_I.Controllers  // added .Controllers to names
             // The anonymous object consists of keys and values
             // The keys should match the parameter names of the method being redirected to
         }
+
+
+        /*
+        Redirecting to another Controller's method:
+        return RedirectToAction("OtherMethodName", "OtherControllerName");  leave off Controller part of Controller name
+        We can also pass arguments when redirecting to a different controller; simply add an anonymous object 
+        as a third argument to the RedirectToAction method.
+        */
+
+
+        /*
+        All the return data types we used above inherit from IActionResult, so all the return types above
+        could be changed to IActionResult. This is useful if the return data types could be different based
+        on an if/else statement.
+        */
+
+
+        // Return Json example:
+        [HttpGet("json_example/{username}/{location}")]
+        public JsonResult JsonExample(string username, string location)
+        {
+            var response = new{user=username,place=location};
+            return Json(response);
+        }
+
+
+        // ViewBag example:
+        [HttpGet("viewbag_example")]
+        public IActionResult ViewBagExample()
+        {
+            // Here we assign the value "Hello World!" to the property .Example
+            // Property names are arbitrary and can be whatever you like
+            ViewBag.Example = "Hello World!";
+            return View("Index");
+        }
+
+        /*
+        One way we can send data from our controllers to our views is with an object called ViewBag. 
+        ViewBag is a dictionary that only persists over one view return, and does not persist across redirects, 
+        so it must be set in the method rendering the view we're sending data to. To use ViewBag, we define 
+        properties and assign values to them.
+        */
+
+
     }
 }
