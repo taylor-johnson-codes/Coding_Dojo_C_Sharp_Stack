35232e1e11cdb2e63ab0df935eaef6baacd10758
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-08 13:19:55 -0700

    added HomeController, Index, and style.css

---
+++ 3_ASP.NET_Core/1_ASP_MVC_I/Portfolio_II/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_II.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public string Index()
+        {
+            return "This is my Index!";
+        }
+
+        [HttpGet("projects")]
+        public string Projects()
+        {
+            return "These are my projects!";
+        }
+
+        [HttpGet("contact")]
+        public string Contact()
+        {
+            return "This is my contact info!";
+        }
+    }
+}
