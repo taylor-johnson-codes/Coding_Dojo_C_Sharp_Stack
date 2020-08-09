7ec5466f023ef76bdea84a6040ac996ea4f939f2
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-08 21:46:28 -0700

    working on assignment

--- 3_ASP.NET_Core/2_ASP_MVC_II/ViewModel_Fun/Controllers/HomeController.cs
+++ 3_ASP.NET_Core/2_ASP_MVC_II/ViewModel_Fun/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using ViewModel_Fun.Models;
 
 namespace ViewModel_Fun.Controllers
 {
@@ -11,6 +13,31 @@ namespace ViewModel_Fun.Controllers
             return View(message);
         }
 
+        [HttpGet("numbers")]
+        public IActionResult Numbers()
+        {
+            int[] numbers = new int[] {1,2,3,4,5};
+            return View(numbers);
+        }
 
+        [HttpGet("users")]
+        public IActionResult Users()
+        {
+            List<User> user_list = new List<User>()
+            {
+                new User() {FirstName="Taylor", LastName="Johnson"},
+                new User() {FirstName="Mac", LastName="Dog"},
+            };
+            return View(user_list);
+        }
+
+        [HttpGet("new_user")]
+        public IActionResult NewUser()
+        {
+            User user1 = new User();
+            user1.FirstName = "Taylor";
+            user1.LastName = "Johnson";
+            return View(user1);
+        }
     }
 }
