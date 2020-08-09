f8414b53c245c143d298cff2dffb0281e8e593f2
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-09 13:11:29 -0700

    following demo

--- 3_ASP.NET_Core/2_ASP_MVC_II/Platform_Lecture_ASP_MVC_II/Models/User.cs
+++ 3_ASP.NET_Core/2_ASP_MVC_II/Platform_Lecture_ASP_MVC_II/Models/User.cs
@@ -6,3 +6,9 @@ namespace Platform_Lecture_ASP_MVC_II.Models  // notice .Models was added on her
         public string LastName { get; set; }
     }
 }
+
+// the User class can have all different kinds of data types in it, so when you're sending over the User data type
+// to a cshtml page, you're sending over all the different data types within the User class
+
+// if I just want a model to store data and not be instantiated (i.e. not create a new User),
+// then add 'static' before 'public'
