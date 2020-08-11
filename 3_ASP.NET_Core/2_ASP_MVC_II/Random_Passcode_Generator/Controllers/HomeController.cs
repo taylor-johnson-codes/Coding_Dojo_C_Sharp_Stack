3498630e8b169c879b4dab506253572e4259ea4d
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-10 19:55:22 -0700

    finished assignment

--- 3_ASP.NET_Core/2_ASP_MVC_II/Random_Passcode_Generator/Controllers/HomeController.cs
+++ 3_ASP.NET_Core/2_ASP_MVC_II/Random_Passcode_Generator/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 
@@ -8,7 +9,47 @@ namespace Random_Passcode_Generator.Controllers
         [HttpGet("")]
         public IActionResult Index()
         {
-            return View();
+            Random random = new Random();
+            string charOptions = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] charArray = new char[14];
+            for (int i = 0; i < 14; i++)
+            {
+                charArray[i] = charOptions[random.Next(0, charOptions.Length)];
+            }
+            // alternate: string passcode = string.Join("", charArray);
+            string passcode = new string(charArray);
+            ViewBag.Code = passcode;
+
+            Count();
+            ViewBag.Count = HttpContext.Session.GetInt32("Count");
+            return View("Index");
+        }
+
+        [HttpPost("generate")]
+        public IActionResult Generate()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public void Count()
+        {
+            int? count = HttpContext.Session.GetInt32("Count");
+            if(count == null)
+            {
+                count = 1;
+            }
+            else
+            {
+            count ++;
+            }
+            HttpContext.Session.SetInt32("Count", (int)count);  // key value pair
+        }
+
+        [HttpGet("reset")]
+        public IActionResult Reset()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
     }
 }
