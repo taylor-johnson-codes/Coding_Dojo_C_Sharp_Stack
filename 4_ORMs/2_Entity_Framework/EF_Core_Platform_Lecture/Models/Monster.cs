418337f04f98844e2d8ac8ee1849ed3d807eda57
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-15 20:34:15 -0700

    following demo

--- 4_ORMs/2_Entity_Framework/EF_Core_Platform_Lecture/Models/Monster.cs
+++ 4_ORMs/2_Entity_Framework/EF_Core_Platform_Lecture/Models/Monster.cs
@@ -1,11 +1,25 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace EF_Core_Platform_Lecture.Models
 {
     public class Monster
     {
-        [Required(ErrorMessage = "This is required.")]
-        [MinLength(1)]
-        public string FirstName { get; set; }
+        // [Required(ErrorMessage = "This is required.")]
+        // [MinLength(1)]
+
+        [Key]
+        public int MonsterId { get; set; }
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
 }
+
+/*
+Model classes such as these, that bear the responsibility of representing an entry in a database table, 
+are typically referred to as an "Entity". In this Entity class, we will provide Auto-Implemented Properties 
+that will be used by Entity Framework to "map" to Columns in the associated table. 
+*/
