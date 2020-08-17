b4f2d0444716213990d857042dd1baede734e63c
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-17 15:18:56 -0700

    finished assignment

--- 4_ORMs/2_Entity_Framework/Login_and_Registration/Controllers/HomeController.cs
+++ 4_ORMs/2_Entity_Framework/Login_and_Registration/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Login_and_Registration.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace Login_and_Registration.Controllers
 {
@@ -20,7 +22,84 @@ namespace Login_and_Registration.Controllers
         [HttpGet("")]
         public IActionResult Index()
         {
+            return View("Index");
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(User newUser)
+        {
+            if(ModelState.IsValid)
+            {
+                if(db.Users.Any(u => u.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "Email already registered!");
+                }
+            }
+
+            if(ModelState.IsValid == false)
+            {
+                return View("Index");
+            }
+
+            PasswordHasher<User> hasher = new PasswordHasher<User>();
+            newUser.Password = hasher.HashPassword(newUser, newUser.Password);
+
+            db.Users.Add(newUser);
+            db.SaveChanges();
+            
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            HttpContext.Session.SetString("UserName", newUser.FirstName);
+            return RedirectToAction("Success");
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginUser loginUser)
+        {
+            string genericErrorMsg = "Invalid email or password!";
+
+            if(ModelState.IsValid == false)
+            {
+                return View("Index");
+            }
+
+            User dbUser = db.Users.FirstOrDefault(u => u.Email == loginUser.LoginEmail);
+
+            if(dbUser == null)
+            {
+                ModelState.AddModelError("LoginEmail", genericErrorMsg);
+                return View("Index");
+            }
+
+            PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
+            PasswordVerificationResult pwCompareResult = hasher.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+            
+            if(pwCompareResult == 0)  // 0 means password entered doesn't match password in DB
+            {
+                ModelState.AddModelError("LoginEmail", genericErrorMsg);
+                return View("Index");
+            }
+
+            HttpContext.Session.SetInt32("UserId", dbUser.UserId);
+            HttpContext.Session.SetString("UserName", dbUser.FirstName);
+            return RedirectToAction("Success");
+        }
+
+        [HttpGet("success")]
+        public IActionResult Success()
+        {
+            // if user not logged in, returns user to login/reg page:
+            if(HttpContext.Session.GetInt32("UserId") == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
+        }
+
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
         }
     }
 }
