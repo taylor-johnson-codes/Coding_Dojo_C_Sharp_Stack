2d958aae0d950b17176fa9c2dc999a243c1c3869
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-16 19:32:57 -0700

    spun up CRUDelicious assignment

---
+++ 4_ORMs/2_Entity_Framework/CRUDelicious/Models/Dish.cs
@@ -0,0 +1,35 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDelicious.Models
+{
+    public class Dish
+    {
+        [Key]
+        public int DishId { get; set; }
+
+        [Required(ErrorMessage = "is required")]
+        [MaxLength(45, ErrorMessage = "no longer than 45 characters")]
+        [Display(Name = "First Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "is required")]
+        [MaxLength(45, ErrorMessage = "no longer than 45 characters")]
+        public string Chef { get; set; }
+
+        [Required(ErrorMessage = "is required")]
+        [Range(1, 6, ErrorMessage = "must be from 1 to 5")]
+        public int Tastiness { get; set; }
+
+        [Required(ErrorMessage = "is required")]
+        [Range(1, 5000, ErrorMessage = "must be greater than 0")]
+        public int Calories { get; set; }
+
+        [Required(ErrorMessage = "is required")]
+        [MinLength(1, ErrorMessage = "must be at least 1 character")]
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
