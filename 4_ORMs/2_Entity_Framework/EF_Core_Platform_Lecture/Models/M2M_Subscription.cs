665b4ef7f2ed8f58bb8dcfc6bca9b56887824939
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-19 13:10:08 -0700

    following demo

--- 4_ORMs/2_Entity_Framework/EF_Core_Platform_Lecture/Models/M2M_Subscription.cs
+++ 4_ORMs/2_Entity_Framework/EF_Core_Platform_Lecture/Models/M2M_Subscription.cs
@@ -5,7 +5,7 @@ namespace EF_Core_Platform_Lecture.Models
         public int M2M_SubscriptionId { get; set; }
         public int M2M_PersonId { get; set; }
         public int M2M_MagazineId { get; set; }
-        public M2M_Person M2M_Person { get; set; }
+        public M2M_Person M2M_Subscriber { get; set; }
         public M2M_Magazine M2M_Magazine { get; set; }
     }
 }
